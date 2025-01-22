ca4746fd7fbdef576ec27c1f30be41176f3fea13
Tejac12 <chintalatejavardhan692@gmail.com>
2025-01-22 19:10:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 let move_speed = 3, grativy = 0.5;
 let bird = document.querySelector('.bird');
 let img = document.getElementById('bird-1');
-let sound_point = new Audio('sounds effect/point.mp3');
-let sound_die = new Audio('sounds effect/die.mp3');
+
 
 // getting bird element properties
 let bird_props = bird.getBoundingClientRect();
@@ -52,13 +51,11 @@ function play(){
                     message.innerHTML = 'Game Over'.fontcolor('red') + '<br>Press Enter To Restart';
                     message.classList.add('messageStyle');
                     img.style.display = 'none';
-                    sound_die.play();
-                    return;
+                     return;
                 }else{
                     if(pipe_sprite_props.right < bird_props.left && pipe_sprite_props.right + move_speed >= bird_props.left && element.increase_score == '1'){
                         score_val.innerHTML =+ score_val.innerHTML + 1;
-                        sound_point.play();
-                    }
+                    }                    }
                     element.style.left = pipe_sprite_props.left - move_speed + 'px';
                 }
             }
